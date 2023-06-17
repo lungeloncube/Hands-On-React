a60a342e83b208872d75fe373865552f7bb0aca6
Lungelo Ncube <lungeloncube@gmail.com>
2023-06-17 12:49:02 +0200

    classes commit

--- hands-on-react/src/App.js
+++ hands-on-react/src/App.js
@@ -1,11 +1,22 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+class Welcome extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return <h1>Meet us {this.props.name}</h1>
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+<Welcome name="Lungelo"/>
+<Welcome  name="Lungelo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
